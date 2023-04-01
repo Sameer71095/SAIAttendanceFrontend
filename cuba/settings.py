2f596c830250d929dd6bf238ef964dc93f764800
Sameer71095 <51756695+Sameer71095@users.noreply.github.com>
2023-04-01 14:50:33 +0400

    fixes

--- cuba/settings.py
+++ cuba/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_56tytpc0wt(mx7r)hh2dznbrf+w0)n890dq579jegw5o^2*15'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.clockspotter.com','clockspotter.com','*']
 
@@ -122,12 +123,17 @@ STATICFILES_FINDERS = [
     'sass_processor.finders.CssFinder',
 ]
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/cuba/cubaapp/static/'
 
 STATIC_ROOT = BASE_DIR / 'cubaapp/static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'cubaapp/static/assets'),
+]
+
 
 SASS_PROCESSOR_ROOT = BASE_DIR / 'static'
 
